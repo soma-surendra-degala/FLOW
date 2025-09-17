c5a17069539774914aeb738f315e6d7b24570c04
Surendra <somasurendra12@gmail.com>
2025-09-17 23:10:51 +0530

    Done

--- Angular/src/app/shared/pipes/safe-url.pipe.ts-pipe.ts
+++ Angular/src/app/shared/pipes/safe-url.pipe.ts-pipe.ts
@@ -2,18 +2,12 @@ import { Pipe, PipeTransform } from '@angular/core';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Pipe({
-  name: 'fileUrl',
-  standalone: true, // ✅ important so it can be imported in standalone components
+  name: 'safeUrl'
 })
 export class SafeUrlPipe implements PipeTransform {
   constructor(private sanitizer: DomSanitizer) {}
 
-  transform(file: File | string): SafeResourceUrl {
-    if (file instanceof File) {
-      return this.sanitizer.bypassSecurityTrustResourceUrl(
-        URL.createObjectURL(file)
-      );
-    }
-    return this.sanitizer.bypassSecurityTrustResourceUrl(file);
+  transform(url: string): SafeResourceUrl {
+    return this.sanitizer.bypassSecurityTrustResourceUrl(url);
   }
 }
